0bf1052eb5a2a32cdca683d6151c08ea4049035c
MMinusOne <michael.business.lb@proton.me>
2025-09-26 22:35:27 +0300

    file path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod tokens;
+mod lexer;
+mod errors; 
+
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file = match args.get(1) {
+        Some(file_path) => file_path,
+        None => panic!("No file path provided"),
+    };
+
     
+
+    println!("{file}")
 }
